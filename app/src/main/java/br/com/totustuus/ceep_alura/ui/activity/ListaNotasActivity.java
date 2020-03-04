e8075d7f11e536e3b927bfa28399d322cc5467e0
Thiago Machado <ti-thiago@outlook.com>
2020-03-04 17:17:12 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/totustuus/ceep_alura/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,15 @@
+package br.com.totustuus.ceep_alura.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.totustuus.ceep_alura.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
